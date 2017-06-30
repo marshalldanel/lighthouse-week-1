6c2c10e30729dc505a6add5f653ffd1cef2eb1ee
Marshall <marshalldanel@gmail.com>
2017-06-30 21:46:21 +0000

    Fixed indentation

--- day-1/argv.js
+++ day-1/argv.js
@@ -1,20 +1,20 @@
- var myArgs = require('optimist').argv,
-     help = 'This would be a great place for real help information.';
+var myArgs = require('optimist').argv;
+var help = 'This would be a great place for real help information.';
 
- if ((myArgs.h)||(myArgs.help)) {
-   console.log(help);
-   process.exit(0);
- }
+if ((myArgs.h) || (myArgs.help)) {
+  console.log(help);
+  process.exit(0);
+}
 
- switch (myArgs._[0]) {
-   case 'insult':
-     console.log(myArgs.n || myArgs.name, 'smells quite badly.');
-     break;
-   case 'compliment':
-     console.log(myArgs.n || myArgs.name, 'is really cool.');
-     break;
-   default:
-     console.log(help);
- }
+switch (myArgs._[0]) {
+case 'insult':
+  console.log(myArgs.n || myArgs.name, 'smells quite badly.');
+  break;
+case 'compliment':
+  console.log(myArgs.n || myArgs.name, 'is really cool.');
+  break;
+default:
+  console.log(help);
+}
 
- console.log('myArgs: ', myArgs);
+console.log('myArgs: ', myArgs);
